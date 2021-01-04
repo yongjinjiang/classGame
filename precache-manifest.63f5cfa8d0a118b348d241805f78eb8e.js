99d07366c0db6a3108e1f053d59f00f0dbc8dd13
jagdeepsingh <jagdeepsingh.125k@gmail.com>
2021-01-04 03:15:19 -0600

    Updates

---
+++ precache-manifest.63f5cfa8d0a118b348d241805f78eb8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5186f1299095026778331964bcfe823",
+    "url": "/classGame/index.html"
+  },
+  {
+    "revision": "e28444d3109b607e9356",
+    "url": "/classGame/static/css/main.0a39a5e5.chunk.css"
+  },
+  {
+    "revision": "9b521c1b9b0ef19be417",
+    "url": "/classGame/static/js/2.1dd8058c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/classGame/static/js/2.1dd8058c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e28444d3109b607e9356",
+    "url": "/classGame/static/js/main.3f633595.chunk.js"
+  },
+  {
+    "revision": "d49e40e5037e0eaf5046",
+    "url": "/classGame/static/js/runtime-main.13c55bcf.js"
+  }
+]);
